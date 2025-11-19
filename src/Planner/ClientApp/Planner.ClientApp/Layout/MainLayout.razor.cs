7a82e1e256c2659f8e27a6d5686f921271f9490b
Todd Lang <todd.lang@d2l.com>
2025-11-19 15:18:33 -0500

    Roughing in navigation

---
+++ src/Planner/ClientApp/Planner.ClientApp/Layout/MainLayout.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Planner.ClientApp.Layout;
+
+public class MainLayoutBase : LayoutComponentBase {
+
+	public required MudThemeProvider ThemeProvider { get; set; }
+	public bool IsDarkMode { get; set; }
+	public bool IsDrawerOpen { get; set; }
+
+	protected override async Task OnAfterRenderAsync(
+		bool firstRender
+	) {
+		if( firstRender ) {
+			await ThemeProvider.WatchSystemDarkModeAsync( OnSystemDarkModeChanged );
+			IsDarkMode = await ThemeProvider.GetSystemDarkModeAsync();
+
+			StateHasChanged();
+		}
+	}
+
+	protected void DrawerToggle() {
+		IsDrawerOpen = !IsDrawerOpen;
+	}
+
+	private Task OnSystemDarkModeChanged(
+		bool newValue
+	) {
+		IsDarkMode = newValue;
+		return Task.CompletedTask;
+	}
+}
